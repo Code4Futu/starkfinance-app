b752f822e3a8c040254e3bea3918febac12ba274
noobmdev <noobmdev@gmail.com>
2023-12-11 17:28:16 +0700

    add new launchpad

--- src/app/mock.ts
+++ src/app/mock.ts
@@ -1,5 +1,47 @@
 export const launchpadMocks = [
 	{
+		name: "Launchpad 2",
+		address:
+			"0x24040be00c551196058396e197e90fb1a26cf92a13508695dce8c034e9e6fba",
+		nft: "0x13d22d5ca6dc992c68808c7b8c0a9304bf514a564e82b9e6fb35a7b954aecc5",
+		logo: undefined,
+		chain: "starknet",
+		tokenRaise: {
+			symbol: "SFN",
+			address:
+				"0x3f327365c803ee53491b73d82770e59643736608e077cea589685647ba2b1f8",
+			decimal: 18,
+		},
+		tokenSale: {
+			symbol: "TEST",
+			address:
+				"0x362cb503715420a537c16419001174476bf44b4728eec14117763481ffaa06f",
+			decimal: 18,
+		},
+		type: "public",
+		start: 1702288800,
+		end: 1703152800,
+		totalSale: "1000000000000000000000000",
+		totalRaise: "500000000000000000000000",
+		minCommit: "10000000000000000000",
+		maxCommit: "100000000000000000000000",
+		socials: {
+			x: undefined,
+			telegram: undefined,
+			discord: undefined,
+			medium: undefined,
+			github: undefined,
+		},
+		introVideo: undefined,
+		desc: `
+		Starkfinance is an all-in-one Incubation Hub with a full stack DeFi platform across blockchain networks. We provide exclusive services including IDO Launchpad, NFT Auction, Marketplace, and Exchange.
+
+		We have plans to combine these outstanding features together to promote the development of web3 industry to everyone. In the future, we will expand on multichain and build a Starkfinance ecosystem.Starkfinance is an all-in-one Incubation Hub with a full stack DeFi platform across blockchain networks. We provide exclusive services including IDO Launchpad, NFT Auction, Marketplace, and Exchange.
+
+		We have plans to combine these outstanding features together to promote the development of web3 industry to everyone. In the future, we will expand on multichain and build a Starkfinance ecosystem.
+		`,
+	},
+	{
 		name: "Test Launchpad 1",
 		address: "0xe0a91adb046748fcac60202d4ae8b2252eee30ce5cb63b5ad324d5b3cf2d58",
 		nft: "0x13d22d5ca6dc992c68808c7b8c0a9304bf514a564e82b9e6fb35a7b954aecc5",
