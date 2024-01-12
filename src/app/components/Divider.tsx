e9c105c1747f86893cd14a397cf7f0dd61fd0502
Manh Tran <55099264+noobmdev@users.noreply.github.com>
2024-01-12 23:05:22 +0700

    Revert "chore: change to new drawer"

--- src/app/components/Divider.tsx
+++
@@ -1,5 +0,0 @@
-import clsx from "clsx";
-
-export const Divider = ({ className }: { className?: string }) => {
-  return <div className={clsx("h-[1px] w-full bg-[#2D313E]", className)}></div>;
-};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,51 +4,50 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { StarknetProvider } from "./components/starknet-provider";
 import Link from "next/link";
-import Menu from "./components/menu/Menu";
+import Menu from "./components/Menu";
 import Head from "next/head";
-import { useEffect, useState } from "react";
 
 export const metadata: Metadata = {
-  title: "StarkFinance App",
-  icons: [
-    { rel: "icon", url: "/logo.png" },
-    { rel: "apple-touch-icon", url: "/apple-icon.png" },
-  ],
-  // description: "StarkFinance Launchpad description",
+	title: "StarkFinance App",
+	icons: [
+		{ rel: "icon", url: "/logo.png" },
+		{ rel: "apple-touch-icon", url: "/apple-icon.png" },
+	],
+	// description: "StarkFinance Launchpad description",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-      </head>
-      <body>
-        <StarknetProvider>
-          <Menu>{children}</Menu>
-        </StarknetProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+			</head>
+			<body>
+				<StarknetProvider>
+					<Menu>{children}</Menu>
+				</StarknetProvider>
+			</body>
+		</html>
+	);
 }
